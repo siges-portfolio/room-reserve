81d4ba8cc0f53543e7ae7be2aa0b5b90ddbe0ebe
Siges <xomu4557@gmail.com>
2025-12-20 00:25:26 +0200

    ---

--- src/app/modules/client/pages/dashboard/dashboard-schedule.component.ts
+++ src/app/modules/client/pages/dashboard/dashboard-schedule.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PhoneInputComponent } from '@shared/components/phone-input/phone-input.component';
 
 @Component({
   standalone: true,
   selector: 'dashboard-schedule',
   templateUrl: './dashboard-schedule.component.html',
-  imports: [
-    ReactiveFormsModule,
-    PhoneInputComponent,
-  ],
+  imports: [ReactiveFormsModule],
   styleUrls: ['./dashboard-schedule.component.scss'],
 })
 export class DashboardScheduleComponent {}
